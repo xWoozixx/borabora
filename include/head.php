d62ea341e73c3724e03c54f29d817bc7405e8afd
aroyer <adrien.royer140203@gmail.com>
2023-04-28 16:58:03 +1100

    fichiers php ver 0.1

--- include/head.php
+++ include/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" media="screen" href="/css/reset.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/css/grid_12.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="/css/style.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/grid_12.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
 <link href='http://fonts.googleapis.com/css?family=Lato:300italic' rel='stylesheet' type='text/css' />
 <script src="/js/jquery-1.7.min.js"></script>
 <script src="/js/jquery.easing.1.3.js"></script>
