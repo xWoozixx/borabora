b90cb375066b7a9a4bdd8cbe0cb587f736f344b8
xWoozixx <r.jauneau13@gmail.com>
2023-05-05 14:15:50 +1100

    vide

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('ul.menu li a, .text-1, h2, .text-3, .date strong, .button, .soc-icons span', { fontFamily: 'Bilbo', hover:true});
-Cufon.replace('.text-3a', { fontFamily: 'Bharatic-Font(W)', hover:true});
-Cufon.replace('.text-1a', { fontFamily: 'Redressed', hover:true});
